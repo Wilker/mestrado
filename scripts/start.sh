59ad9a71669bbc754dc91b13a2d3f1355bfaba9c
Wilker <Wilker.rj@gmail.com>
2019-08-13 18:27:06 -0300

    arquivos de configurção sendo gerados corretamente

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'Criando Cluster atomix'
 echo 'Criando arquivos de configuração do cluster'
-
+VAR=""
 for i in $(seq 1 $1)
 do
 
@@ -12,38 +12,39 @@ do
     address: "172.21.0.`expr $i + 1`:5679"
   }
   discovery {
-    type: bootstrap
-    nodes.1 {
-      id: biscoito-1
-      address: "172.21.0.2:5679"
-    }
-    nodes.2 {
-      id: biscoito-2
-      address: "172.21.0.3:5679"
-    }
-    nodes.3 {
-      id: biscoito-3
-      address: "172.21.0.4:5679"
-    }
-  }
-}
- 
+    type: bootstrap" > "atomix-$i.conf"
+
+VAR="${VAR}biscoito-1"
+
+for j in $(seq 1 $1)
+do 
+    if [ $1 -gt 1 ] && [ $j -lt `expr $1 - 1` ]
+    then
+            VAR="${VAR},biscoito-`expr $j + 1`"
+    fi
+    echo "      nodes.$j {
+      id: biscoito-$j
+      address: "172.21.0.`expr $j + 2`:5679"
+    }" >> "atomix-$i.conf"
+done
+
+VAR="${VAR%"${VAR##*[![:space:]]}"}"   
+
+echo "  }
+}" >> "atomix-$i.conf"
+    echo "
 management-group {
   type: raft
   partitions: 1
   storage.level: disk
-  members: [biscoito-1,biscoito-2,biscoito-3]
+  members: [$VAR]
 }
  
 partition-groups.raft {
   type: raft
   partitions: 1
   storage.level: disk
-  members: [biscoito-1,biscoito-2,biscoito-3]
-} " > "atomix-$i.conf"
+  members: [$VAR]
+} " >> "atomix-$i.conf"
 done
-
-
-
-
 # sudo docker run --net rede-onos --ip 172.18.0.2 -it --name atomix1 --hostname atomix-1 -v caminho_para_o_arquivo_atomix https://atomix.io/1:/etc/atomix/conf atomix/atomix:3.0.7 --config /etc/atomix/conf/atomix.conf --ignore-resources
