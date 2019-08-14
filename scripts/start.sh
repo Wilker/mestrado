b5df6ef6bfaaa4c9f089db44a48b2f583995408c
Wilker <Wilker.rj@gmail.com>
2019-08-14 14:58:20 -0300

    Corrigido IP dos arquivos de configuração

--- scripts/start.sh
+++ scripts/start.sh
@@ -15,7 +15,7 @@ do
   cluster-id: onos
   node {
     id: biscoito-$i
-    address: \"172.21.0.`expr $i + 1`:5679\"
+    address: \"172.18.0.`expr $i + 1`:5679\"
   }
   discovery {
     type: bootstrap" > "atomix-$i.conf"
@@ -30,7 +30,7 @@ do
     fi
     echo "      nodes.$j {
       id: biscoito-$j
-      address:  \"172.21.0.`expr $j + 2`:5679\"
+      address: \"172.18.0.`expr $j + 1`:5679\"
     }" >> "atomix-$i.conf"
 done
 
