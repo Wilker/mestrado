c138dee6229f3288cfab6eb545e4bddbc71d8f28
Wilker <Wilker.rj@gmail.com>
2019-08-21 18:38:23 -0300

    Script coleta as interfaces virtuais e associa aos nomes dos conteiners

---
+++ scripts/vehtfinder.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for container in $(docker ps -q); do
+    iflink=`docker exec -it $container bash -c 'cat /sys/class/net/eth0/iflink'`
+    iflink=`echo $iflink|tr -d '\r'`
+    veth=`grep -l $iflink /sys/class/net/veth*/ifindex`
+    veth=`echo $veth|sed -e 's;^.*net/\(.*\)/ifindex$;\1;'`
+    echo $(sudo docker ps -aq --format "table {{.ID}}\t{{.Names}}" -f id=$container):$veth | cut -c20-100
+done
