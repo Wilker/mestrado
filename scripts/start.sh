d3084f28427e20049cfcc18c10c8161755e0f15f
Wilker <Wilker.rj@gmail.com>
2019-08-21 14:38:39 -0300

    Adicionado criação de script Mininet e execução em terminal separado

--- scripts/start.sh
+++ scripts/start.sh
@@ -93,7 +93,7 @@ echo "Deixando o diretório de arquivos de configuração"
 cd "../"
 DIR=`pwd`
 echo $DIR
-echo "Executando intâncias de cluster atomix"
+echo "Executando intâncias de cluster Atomix"
 
 for i in $(seq 1 $1)
 do 
@@ -116,3 +116,99 @@ do
   exec bash\""
 done
 
+echo "Criando script mininet"
+echo "."
+echo "."
+echo "."
+
+echo "from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+
+def myNetwork():
+
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='10.0.0.0/8')
+
+    info( '*** Adding controllers\n' )" > myTopology.py
+  for i in $(seq 1 $1)
+  do
+  echo "    c$i=net.addController(name='c$i',
+                      controller=RemoteController,
+                      ip='172.18.0.`expr $i + $1`',
+                      protocol='tcp',
+                      port=6653)
+  " >> myTopology.py
+
+  done
+  CONTROLLERS=""
+  for i in $(seq 1 $1)
+  do
+    CONTROLLERS=$CONTROLLERS"c$i"
+    if [ $i -lt $1 ]
+    then 
+      CONTROLLERS=$CONTROLLERS","
+    fi
+  done
+  echo "    info( '*** Add switches\n')
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
+    s3 = net.addSwitch('s3', cls=OVSKernelSwitch)
+    s4 = net.addSwitch('s4', cls=OVSKernelSwitch)
+
+    info( '*** Add hosts\n')
+    h1 = net.addHost('h1', cls=Host, ip='10.0.0.2', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='10.0.0.3', defaultRoute=None)
+
+    info( '*** Add links\n')
+    net.addLink(h1, s1)
+    net.addLink(h2, s4)
+
+    net.addLink(s1, s2)
+    net.addLink(s1, s3)
+    
+    net.addLink(s2, s4)
+    net.addLink(s3, s4)
+    
+    info( '*** Starting network\n')
+    net.build()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        info(controller)
+        controller.start()
+
+    info( '*** Starting switches\n')
+    net.get('s1').start([$CONTROLLERS])
+    net.get('s2').start([$CONTROLLERS])
+    net.get('s3').start([$CONTROLLERS])
+    net.get('s4').start([$CONTROLLERS])
+
+
+    info( '*** Post configure switches and hosts\n')
+
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()" >> myTopology.py
+
+
+echo "Iniciando Mininet"
+echo "."
+echo "."
+echo "."
+
+CMD="sudo python myTopology.py"
+  gnome-terminal -e "bash -c 
+  \"sudo docker rm onos$i;
+  echo -e '$CMD';
+  $CMD;
+  exec bash\""
