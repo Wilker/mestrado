8d714c78b6b6bd2bf80c75e5b215b47e0f420415
Wiker de Oliveira Delfino <Wilker.rj@gmail.com>
2020-02-18 16:33:25 -0300

    Update inicializador de testes

--- scripts/testes_automatizados/run-tests.sh
+++ scripts/testes_automatizados/run-tests.sh
@@ -4,36 +4,14 @@ C1='192.168.2.11'
 C2='192.168.2.12'
 
 #executa o comando direto no karaf
-
 # ./bin/client apps
+
 log(){
     date +%s | tee -a teste_script.txt
     echo -e $1 | tee -a teste_script.txt
 }
-#1 - sincronizar o tempo entre todas as máquinas
-# atualmente ntp no servidor da teasa-10
-
-# 1 - ssh no servidor
 
 
-#CMD="ssh wilker@'$C1 ./init_server.sh'"
-CMD=ssh wilker@$SERVER whoami
-
-gnome-terminal -e "bash -c
-\"$(log 'Conectando ao servidor');
-$(log 'Iniciando testes');
-echo -e '$CMD';
-$CMD;
-exec bash\""
-
-
-# ssh wilker@'$C1 ./init_server.sh'
-
-
-# 2 - ssh do servidor para o controlador 1
-# 3 - ssh do servidor para o controlador 2
-# 4 - executar mininet com a topologia no controlador no servidor
-# 5 - executar atomix no controlador 1
-# 6 - executar atomix no controlador 2
-# 7 - executar atomix no servidor
-# 8 - iniciar o onos no 
+log 'Conectando ao servidor';
+log 'Iniciando testes';
+ssh wilker@'$SERVER' ./init_server.sh
