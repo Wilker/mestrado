8f645ce78dc9d291b88b55577daee2bc6f169c21
Wilker <Wilker.rj@gmail.com>
2020-02-18 19:25:34 -0300

    Update Topologia

--- scripts/testes_automatizados/myTopology.py
+++ scripts/testes_automatizados/myTopology.py
@@ -8,38 +8,38 @@ from mininet.log import setLogLevel, info
 from mininet.link import TCLink, Intf
 from subprocess import call
 
-
 def myNetwork():
-    net = Mininet(topo=None,
-                  build=False,
-                  ipBase='172.18.0.0/16')
 
-    info('*** Adding controllers\n')
-    c1 = net.addController(name='c1',
-                           controller=RemoteController,
-                           ip='172.18.0.4',
-                           protocol='tcp',
-                           port=6653)
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='192.168.0.0/16')
 
-    c2 = net.addController(name='c2',
-                           controller=RemoteController,
-                           ip='172.18.0.5',
-                           protocol='tcp',
-                           port=6653)
-
-    info('*** Add switches\n')
+    info( '*** Adding controllers\n' )
+    c1=net.addController(name='c1',
+                      controller=RemoteController,
+                      ip='192.168.2.11',
+                      protocol='tcp',
+                      port=6653)
+  
+    c2=net.addController(name='c2',
+                      controller=RemoteController,
+                      ip='192.168.2.12',
+                      protocol='tcp',
+                      port=6653)
+  
+    info( '*** Add switches\n')
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
     s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
     s3 = net.addSwitch('s3', cls=OVSKernelSwitch)
     s4 = net.addSwitch('s4', cls=OVSKernelSwitch)
 
-    info('*** Add hosts\n')
-    h1 = net.addHost('h1', cls=Host, ip='172.18.0.10', defaultRoute=None)
-    h2 = net.addHost('h2', cls=Host, ip='172.18.0.11', defaultRoute=None)
-    h3 = net.addHost('h3', cls=Host, ip='172.18.0.12', defaultRoute=None)
-    h4 = net.addHost('h4', cls=Host, ip='172.18.0.13', defaultRoute=None)
+    info( '*** Add hosts\n')
+    h1 = net.addHost('h1', cls=Host, ip='192.168.0.10', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='192.168.0.11', defaultRoute=None)
+    h3 = net.addHost('h3', cls=Host, ip='192.168.0.12', defaultRoute=None)
+    h4 = net.addHost('h4', cls=Host, ip='192.168.0.13', defaultRoute=None)
 
-    info('*** Add links\n')
+    info( '*** Add links\n')
     net.addLink(h1, s1)
     net.addLink(h2, s4)
     net.addLink(h3, s3)
@@ -47,31 +47,31 @@ def myNetwork():
 
     net.addLink(s1, s2)
     net.addLink(s1, s3)
-
+    
     net.addLink(s2, s4)
     net.addLink(s3, s4)
-
-    info('*** Starting network\n')
+    
+    info( '*** Starting network\n')
     net.build()
-    info('*** Starting controllers\n')
+    info( '*** Starting controllers\n')
     for controller in net.controllers:
         info(controller)
         controller.start()
 
-    info('*** Starting switches\n')
-    net.get('s1').start([c1, c2])
-    net.get('s2').start([c1, c2])
-    net.get('s3').start([c1, c2])
-    net.get('s4').start([c1, c2])
+    info( '*** Starting switches\n')
+    net.get('s1').start([c1,c2])
+    net.get('s2').start([c1,c2])
+    net.get('s3').start([c1,c2])
+    net.get('s4').start([c1,c2])
 
-    info('*** Post configure switches and hosts\n')
 
-    h1.cmd("tcpreplay -i h1-eth0 -l 100 -p 20 arp-replay.pcapng &")
-#    h1.cmd("wireshark &")
+    info( '*** Post configure switches and hosts\n')
+
+    net.pingAll()
+    
     CLI(net)
     net.stop()
 
-
 if __name__ == '__main__':
-    setLogLevel('info')
+    setLogLevel( 'info' )
     myNetwork()
