03909b52e66d9626b619a5ddfc6b087a751ed148
Wilker <Wilker.rj@gmail.com>
2020-08-06 13:51:21 -0300

    Capturando primeiro pacote syn

--- scripts/processa_resultados/processa_resultados.py
+++ scripts/processa_resultados/processa_resultados.py
@@ -34,9 +34,10 @@ def process_folder(folder, writable):
     base_time = get_time(0, get_last_packet_out(master))
     switch_port = get_switch_port(slave)
     multipartcount = get_multi_part_count(slave, MULTIPART_REPLY, get_first_role_reply(slave, switch_port),
-                              get_first_packet_out(slave, switch_port),switch_port)
+                             get_first_packet_out(slave, switch_port), switch_port)
     writable.write('{},'.format(folder[-2::]))
     writable.write('{},'.format(get_time(base_time, get_fist_fin_packet(master))))
+    writable.write('{},'.format(get_time(base_time, get_first_syn_packet(slave, get_cap_ip(slave)))))
     writable.write('{},'.format(get_time(base_time, get_first_role_request(slave, switch_port))))
     writable.write('{},'.format(get_time(base_time, get_first_role_reply(slave, switch_port))))
     writable.write('{},'.format(get_time(base_time, get_first_multipart_request(slave, switch_port))))
@@ -45,6 +46,9 @@ def process_folder(folder, writable):
     writable.write('{}\n'.format(multipartcount))
     os.chdir('..')
 
+
+def get_cap_ip(pcap):
+    return '192.168.2.11' if "RAV1" in pcap else '192.168.2.12'
 
 def get_master_and_slave(pcaps):
     master = ''
@@ -125,9 +129,13 @@ def get_fist_fin_packet(pcap):
     return line
 
 
-def get_first_syn_packe(pcap):
-    cmd = "tshark -2 -r {} -R '(tcp.flags.syn == 1) && tcp.srcport == 6653' -e frame.time_epoch -e tcp.srcport -e tcp.dstport -Tfields   > {}".format(
-        pcap, FILE_NAME)
+def get_first_syn_packet(pcap, ip):
+    if ip[-1] == '2':
+        ip = ip[:-1]+'1'
+    else:
+        ip = ip[:-1]+'2'
+    cmd = "tshark -2 -r {} -R '(tcp.flags.syn == 1) && tcp.dstport == 6653 && (ip.dst=={})' -e frame.time_epoch -e tcp.srcport -e tcp.dstport -Tfields   > {}".format(
+        pcap,ip, FILE_NAME)
     os.system(cmd)
     line = open(FILE_NAME, "r").readline()
     os.remove(FILE_NAME)
@@ -166,6 +174,7 @@ def extract_results(path_to_folder):
     with open("results-{}-test.csv".format(folder), 'w') as file:
         file.write('FOLDER,')
         file.write('FIN,')
+        file.write('SYN,')
         file.write('ROLE REQUEST,')
         file.write('ROLE REPLY,')
         file.write('MULTIPART REQUEST,')
