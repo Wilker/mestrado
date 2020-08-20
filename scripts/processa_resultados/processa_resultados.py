84ef6d5f59bcb142efcb0dab6cd07771e3d104a1
Wilker <Wilker.rj@gmail.com>
2020-08-20 18:38:04 -0300

    Extraindo mais infos das capturas

--- scripts/processa_resultados/processa_resultados.py
+++ scripts/processa_resultados/processa_resultados.py
@@ -30,19 +30,55 @@ def process_folder(folder, writable):
     pcaps = list(filter(lambda k: 'pcap' in k, onlyfiles))
     os.chdir(folder)
     print(os.getcwd())
+
     master, slave = get_master_and_slave(pcaps)
     base_time = get_time(0, get_last_packet_out(master))
     switch_port = get_switch_port(slave)
+    # first_role_reply_slave = get_first_role_reply(slave, switch_port)
+    # first_packet_out_slave = get_first_packet_out(slave, switch_port)
+    #
+    # x = get_all(MULTIPART_REQUEST, first_role_reply_slave, first_packet_out_slave, slave, switch_port)
+    # for i in x:
+    #     print(str(get_time(base_time, i)), end=',')
+    #
+    # print("\n--------------------")
+    #
+    # x = get_all(MULTIPART_REPLY, first_role_reply_slave, first_packet_out_slave, slave, switch_port)
+    # for i in x:
+    #     print(str(get_time(base_time, i)), end=',')
+    #
+    # print("")
+
+    ####  cap times
+    fist_fin_packet = get_fist_fin_packet(master)
+    first_syn_packet = get_first_syn_packet(slave, get_cap_ip(slave))
+    first_role_request = get_first_role_request(slave, switch_port)
+    first_role_reply = get_first_role_reply(slave, switch_port)
+    first_multipart_request = get_first_multipart_request(slave, switch_port)
+    first_multipart_reply = get_first_multipart_reply(slave, switch_port)
+    first_packet_out = get_first_packet_out(slave, switch_port)
     multipartcount = get_multi_part_count(slave, MULTIPART_REPLY, get_first_role_reply(slave, switch_port),
-                             get_first_packet_out(slave, switch_port), switch_port)
+                                          get_first_packet_out(slave, switch_port), switch_port)
+    ####
+
     writable.write('{},'.format(folder[-2::]))
-    writable.write('{},'.format(get_time(base_time, get_fist_fin_packet(master))))
-    writable.write('{},'.format(get_time(base_time, get_first_syn_packet(slave, get_cap_ip(slave)))))
-    writable.write('{},'.format(get_time(base_time, get_first_role_request(slave, switch_port))))
-    writable.write('{},'.format(get_time(base_time, get_first_role_reply(slave, switch_port))))
-    writable.write('{},'.format(get_time(base_time, get_first_multipart_request(slave, switch_port))))
-    writable.write('{},'.format(get_time(base_time, get_first_multipart_reply(slave, switch_port))))
-    writable.write('{},'.format(get_time(base_time, get_first_packet_out(slave, switch_port))))
+    writable.write('{},'.format(base_time))
+
+    writable.write('{},'.format(fist_fin_packet.rstrip()))
+    writable.write('{},'.format(first_syn_packet).rstrip())
+    writable.write('{},'.format(first_role_request).rstrip())
+    writable.write('{},'.format(first_role_reply).rstrip())
+    writable.write('{},'.format(first_multipart_request).rstrip())
+    writable.write('{},'.format(first_multipart_reply).rstrip())
+    writable.write('{},'.format(first_packet_out).rstrip())
+
+    writable.write('{},'.format(get_time(base_time, fist_fin_packet)))
+    writable.write('{},'.format(get_time(base_time, first_syn_packet)))
+    writable.write('{},'.format(get_time(base_time, first_role_request)))
+    writable.write('{},'.format(get_time(base_time, first_role_reply)))
+    writable.write('{},'.format(get_time(base_time, first_multipart_request)))
+    writable.write('{},'.format(get_time(base_time, first_multipart_reply)))
+    writable.write('{},'.format(get_time(base_time, first_packet_out)))
     writable.write('{}\n'.format(multipartcount))
     os.chdir('..')
 
@@ -84,11 +120,29 @@ def get_last_packet_out(pcap):
     return last_packet
 
 
+def get_all(packet_type, time_init, time_end, pcap, port):
+    file_name = "tmp"
+    cmd = "tshark -2 -r {} -R '{}' -e frame.time_epoch -e tcp.srcport -e tcp.dstport -Tfields > {}".format(pcap,
+                                                                                                           packet_type,
+                                                                                                           FILE_NAME)
+    os.system(cmd)
+    lines = []
+
+    with open(file_name, "r") as file:
+        for line in file:
+            target_port = line.split()[2] if packet_type == MULTIPART_REQUEST else line.split()[1]
+            if (float(line.split()[0]) > float(time_init)) and (float(line.split()[0]) < float(time_end)) and (
+                    port == target_port):
+                lines.append(line.split()[0])
+    os.remove(file_name)
+    return lines
+
+
 def get_first(packet_type, pcap, port):
     file_name = "tmp"
     cmd = "tshark -2 -r {} -R '{}' -e frame.time_epoch -e tcp.srcport -e tcp.dstport -Tfields > {}".format(pcap,
-                                                                                                              packet_type,
-                                                                                                              FILE_NAME)
+                                                                                                           packet_type,
+                                                                                                           FILE_NAME)
     os.system(cmd)
     first_line = ''
     with open(file_name, "r") as file:
@@ -131,15 +185,15 @@ def get_fist_fin_packet(pcap):
 
 def get_first_syn_packet(pcap, ip):
     if ip[-1] == '2':
-        ip = ip[:-1]+'1'
+        ip = ip[:-1] + '1'
     else:
-        ip = ip[:-1]+'2'
+        ip = ip[:-1] + '2'
     cmd = "tshark -2 -r {} -R '(tcp.flags.syn == 1) && tcp.dstport == 6653 && (ip.dst=={})' -e frame.time_epoch -e tcp.srcport -e tcp.dstport -Tfields   > {}".format(
-        pcap,ip, FILE_NAME)
+        pcap, ip, FILE_NAME)
     os.system(cmd)
     line = open(FILE_NAME, "r").readline()
     os.remove(FILE_NAME)
-    return line
+    return line.split()[0]
 
 
 def get_switch_port(pcap):
@@ -158,7 +212,8 @@ def get_multi_part_count(pcap, packet_type, time_init, time_end, switch_port):
     count = 0
     with open(FILE_NAME, "r") as file:
         for line in file:
-            if (float(line.split()[0]) > float(time_init)) and (float(line.split()[0]) < float(time_end)) and (switch_port ==line.split()[1]):
+            if (float(line.split()[0]) > float(time_init)) and (float(line.split()[0]) < float(time_end)) and (
+                    switch_port == line.split()[1]):
                 count += 1
     os.remove(FILE_NAME)
     return count
@@ -173,6 +228,14 @@ def extract_results(path_to_folder):
     subfolders = [f.path for f in os.scandir('.') if f.is_dir()]
     with open("results-{}-test.csv".format(folder), 'w') as file:
         file.write('FOLDER,')
+        file.write('Base Time,')
+        file.write('FIN - EPOCH,')
+        file.write('SYN - EPOCH,')
+        file.write('ROLE REQUEST - EPOCH,')
+        file.write('ROLE REPLY - EPOCH,')
+        file.write('MULTIPART REQUEST - EPOCH,')
+        file.write('MULTIPART REPLY - EPOCH,')
+        file.write('PACKET OUT - EPOCH,')
         file.write('FIN,')
         file.write('SYN,')
         file.write('ROLE REQUEST,')
